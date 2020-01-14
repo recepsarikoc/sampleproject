fab502e29d83ab852e5462b485d24685a8e20b92
recepsarikoc <36772477+recepsarikoc@users.noreply.github.com>
2020-01-14 09:50:15 +0300

    Update snowball.py
    2 -kodu sadeleştirdim tarih işlemlerini int değerlerle yaptım.

--- snowball.py
+++ snowball.py
@@ -1,44 +1,32 @@
 from datetime import datetime
-s = "20200103"
-s_datetime = datetime.strptime(s, '%Y%m%d')
-print(s_datetime)
-a = [s_datetime,s_datetime,s_datetime,s_datetime,s_datetime,s_datetime]
+
+a = [1,20,30,4,3,2]  # medic i nin son görülme tarihi ile içinde bulunduğumuz günün farkının değeridir
 b = [400, 401, 402, 403, 404, 405]
-c = [400, 401, 402, 411, 412]
+c = [400, 401, 402, 411, 412]  # öne çıkarılacak medic listesi
 d = [['kadıköy', 'maltepe', 'ataşehir'], ['kadıköy', 'üsküdar', 'beykoz'],
      ['kadıköy', 'üsküdar', 'beykoz'], ['kadıköy', 'üsküdar', 'beykoz'], ['kadıköy', 'sancaktepe', 'tuzla'],
      ['kadıköy', 'kartal', 'pendik']]
 # print(d[5][2])
 e = [0, 0, 0, 0, 0, 0]
-f = datetime.now()
-print(f)
-print(type(f))
-i = 0
-j = 0
-queue = []
-e[i] = f - a[i]
-g = e[i].days + 10
-print(g)
+for i in range(0,6):
 
-#hizmet talep edilen bölge
-k = 'kadıköy'
-print(type(e[i]))
+    f = 0 #içinde bulunduğumuz günü temsil eder
+    e[i] = f - a[i]
+    k = 'kadıköy'
 
-#timedelta olduğu için days kullanılıyor
-print(e[i].days)
+    if b[i] in c:
+        e[i] = e[i] + 10
+    m = 0
+    n = 0
+    print(len(d))
+#d iki boyutlu bir dizidir m ve n indisleri ve for döngüsüyle bu dizide dolaşıyor ve kontrollerimi yapıyorum
+    for m in range(0, 6):
+        for n in range(0, 3):
+            if k == d[m][n]:
+                print(True)
+                e[i] = e[i] + 5
+            else:
+                print(False)
 
-#datetime olduğu için day kullanılıyor
-print(s_datetime.day)
-
-if b[i] in c:
-    e[i] = e[i].days + 10
-
-if j < 3:
-
-    if k == d[m][n]:
-        e[i] = e[i] + 5
-    j = j+1
-
-queue.append(e[i])
-
-print(e)
+    print('puanlar')
+    print(e)
